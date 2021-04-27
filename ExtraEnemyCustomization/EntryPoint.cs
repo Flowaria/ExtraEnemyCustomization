f9a61b0ef6a4352716e1adb6df093c398a69e5bb
Flowaria <wnsghgj1234@naver.com>
2021-04-27 18:05:12 +0900

    Simplified Name for better log

--- ExtraEnemyCustomization/EntryPoint.cs
+++ ExtraEnemyCustomization/EntryPoint.cs
@@ -9,7 +9,7 @@ namespace ExtraEnemyCustomization
 {
     //TODO: Refactor the CustomBase to support Phase Setting
 
-    [BepInPlugin("GTFO.EECustomization", "ExtraEnemyCustomization", "0.3.1")]
+    [BepInPlugin("GTFO.EECustomization", "EECustomization", "0.3.1")]
     [BepInProcess("GTFO.exe")]
     [BepInDependency(MTFOGUID, BepInDependency.DependencyFlags.HardDependency)]
     public class EntryPoint : BasePlugin
